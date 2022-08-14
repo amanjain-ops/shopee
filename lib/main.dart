fe74ef5bdd0cb656c16c7974a0d14b337fd7ad93
amanjain-ops <71957781+amanjain-ops@users.noreply.github.com>
2022-08-14 22:59:48 +0530

    getx navigation add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shopee/models/product.dart';
+import 'package:shopee/views/favorite_page.dart';
 import 'package:shopee/views/home_page.dart';
 
 void main() {
@@ -9,13 +12,16 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shopee',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home:  HomePage(),
+      getPages: [
+        GetPage(name: '/fav', page: () =>  const  FavoritePage(),),
+      ],
     );
   }
 }
