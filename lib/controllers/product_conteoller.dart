c751e24cf8ed391fb5bca65fadb70be030ba2954
amanjain-ops <71957781+amanjain-ops@users.noreply.github.com>
2022-08-14 19:22:29 +0530

    add crontroller

---
+++ lib/controllers/product_conteoller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:shopee/models/product.dart';
+import 'package:shopee/services/remote_services.dart';
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productItems = <Product>[].obs;
+
+  @override
+  void onInit() {
+    fetchProduct();
+    super.onInit();
+  }
+
+  void fetchProduct() async {
+    try {
+      isLoading(true);
+      var products = await RemoteServices.fetchProduct();
+      if (products != null) {
+        productItems.value = products;
+      }else{
+        print('hello');
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
