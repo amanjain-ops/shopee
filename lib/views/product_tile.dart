b66447b0dc13e2de4dfd44648cd78cac25de913d
amanjain-ops <71957781+amanjain-ops@users.noreply.github.com>
2022-08-16 15:10:11 +0530

    .

--- lib/views/product_tile.dart
+++ lib/views/product_tile.dart
@@ -4,102 +4,116 @@ import 'package:shopee/models/product.dart';
 import 'package:get/get.dart';
 
 class ProductTile extends StatelessWidget {
-  ProductTile({Key? key, required this.product,}) : super(key: key);
+  const ProductTile({Key? key, required this.product,}) : super(key: key);
   final Product product;
   // int? count = 0;
   static var count = 0.obs;
   @override
   Widget build(BuildContext context) {
     
-    return Card(
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 180,
-                  width: double.infinity,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Image.network(
-                    product.imageLink,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  child: GetX<ProductController>(
-                    builder: (controller) {
-                      return CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: IconButton(
-                          icon: product.isFavorite.value
-                              ? const Icon(Icons.favorite_rounded)
-                              : const Icon(Icons.favorite_border),
-                          onPressed: () {
-                            if(product.isFavorite.value){
-                              ProductController.favItems.remove(product);
-                            }else{
-                              ProductController.favItems.add(product);
-                            }
-                            product.isFavorite.toggle();
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              product.name,
-              maxLines: 2,
-              style: const TextStyle(
-                  fontFamily: 'avenir', fontWeight: FontWeight.w800),
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            if (product.rating != null)
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      product.rating.toString(),
-                      style: const TextStyle(color: Colors.white),
+    return InkWell(
+      onTap: () {
+        Get.toNamed('view',arguments: {
+          'name': product.name,
+          'des': product.description,
+          'image': product.imageLink,
+          'brand': product.brand,
+          'price': product.price,
+          'fav': product.isFavorite,
+          'prod': product,
+        })
+        ;
+      },
+      child: Card(
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 180,
+                    width: double.infinity,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
                     ),
-                    const Icon(
-                      Icons.star,
-                      size: 16,
-                      color: Colors.white,
+                    child: Image.network(
+                      product.imageLink,
+                      fit: BoxFit.cover,
                     ),
-                  ],
-                ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: GetX<ProductController>(
+                      builder: (controller) {
+                        return CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: IconButton(
+                            icon: product.isFavorite.value
+                                ? const Icon(Icons.favorite_rounded)
+                                : const Icon(Icons.favorite_border),
+                            onPressed: () {
+                              if(product.isFavorite.value){
+                                ProductController.favItems.remove(product);
+                              }else{
+                                ProductController.favItems.add(product);
+                              }
+                              product.isFavorite.toggle();
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              '\$${product.price}',
-              style: const TextStyle(fontSize: 32, fontFamily: 'avenir'),
-            ),
-          ],
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                product.name,
+                maxLines: 2,
+                style: const TextStyle(
+                    fontFamily: 'avenir', fontWeight: FontWeight.w800),
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              if (product.rating != null)
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        product.rating.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const Icon(
+                        Icons.star,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                '\$${product.price}',
+                style: const TextStyle(fontSize: 32, fontFamily: 'avenir'),
+              ),
+            ],
+          ),
         ),
       ),
     );
