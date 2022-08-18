9ec5a07f93496dda964613c60345b2e408cf522c
amanjain-ops <71957781+amanjain-ops@users.noreply.github.com>
2022-08-19 00:35:04 +0530

    .

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -4,10 +4,12 @@ import 'package:shopee/models/product.dart';
 class CartController extends GetxController {
   var cartItems = <Product>[].obs;
   int get count => cartItems.length;
+  
   double get totalPrice =>
       cartItems.fold(0, (sum, item) => sum + (item.price as double));
 
   addToCart(Product product) {
+    // cartItems.value = product as List<Product>;
     cartItems.add(product);
   }
 }
